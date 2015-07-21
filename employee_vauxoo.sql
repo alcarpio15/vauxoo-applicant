097bb0ec5cb4a4be4e9de8cb84cfaa2fed68593b
alcarpio15 <alcarpio15@gmail.com>
2015-07-21 13:22:00 -0430

    Carpio: se Modificó employee_vauxoo.sql para propositos del ejercicio sql1.

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -1,15 +1,54 @@
--- Your sql code in this file
--- NOTE: Please, don't add sentence to create database in this script file.
---       You can create database locally to test it.
---       Consider add ';' at end sentence.
+-- "employee_vauxoo.sql" por Pedro A. Carpio
+--
+--
+--Contacteme a traves del Telefono: (416)434-7903,
+--	
+-- 
+-- NOTA: Por favor, no agregue comandos para crear bases de datos en este archivo.
+--       Puede crear base de datos localmente para probarlo.
+--       Considere agregar ';' al comando final.
 
-CREATE TABLE employee (
-);
+-- Creo las Tablas: 'employee' se define de ultimo porque deseo establecer
+-- 					sus relaciones foraneas con otras tablas a la hora de
+--					su creacion.
 
 CREATE TABLE employee_department (
+	id INT NOT NULL,
+	name VARCHAR(64) NOT NULL,
+	description VARCHAR(256),
+	PRIMARY KEY(id)
 );
 
 CREATE TABLE employee_hobby (
 );
 
--- ...
+CREATE TABLE employee (
+	id INT NOT NULL,
+	first_name VARCHAR(32) NOT NULL,
+	last_name VARCHAR(32) NOT NULL,
+	department_id INT  NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY(department_id) REFERENCES employee_department(id)
+	ON DELETE CASCADE
+);
+
+CREATE TABLE has_as_hobby (
+);
+
+
+-- Insertamos Departamentos y Empleados para el Ejercicio sql1
+
+INSERT INTO employee_department (id, name, description) VALUES (1, 'Produccion', ' ');
+INSERT INTO employee_department (id, name, description) VALUES (2, 'Ventas', ' ');
+INSERT INTO employee_department (id, name, description) VALUES (3, 'Soporte Técnico', ' ');
+INSERT INTO employee_department (id, name, description) VALUES (4, 'Contaduria', ' ');
+INSERT INTO employee_department (id, name, description) VALUES (5, 'Recursos Humanos', ' ');
+INSERT INTO employee_department (id, name, description) VALUES (6, 'Comite Directivo', ' ');
+
+--Nombres al Azar, cortesia de BehindTheName.com. Apellidos escogidos por otros métodos.
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES(1, 'Felipe Aurelio', 'Gonzalez', 6);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES(2, 'Eladio Geronimo', 'Villa', 3);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES(3, 'Adela', 'Alvarez Arevalo', 2);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES(4, 'Maritza Nuria', 'Ortiz Quevedo', 5);
+
+
