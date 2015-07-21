fe957d1183507360718f878c029201f3ea7f4a50
alcarpio15 <alcarpio15@gmail.com>
2015-07-21 09:36:35 -0430

    Carpio: Tanto primes.py como calculator.py fueron Modificados según los resultados de la pruebasflake8, pylint y .all_unittest.py

--- calculator.py
+++ calculator.py
@@ -1,25 +1,27 @@
 """
-"primes.py" por Pedro A. Carpio
+"calculator.py" por Pedro A. Carpio
 
-Contacteme a través del Teléfono: (416)434-7903, o el Correo Electrónico: alcarpio15@gmail.com
+Contacteme a traves del Telefono: (416)434-7903,
+ o el Correo Electronico: alcarpio15@gmail.com
 """
 
 
 class CalculatorClass(object):
     """
-    Para propósitos del Ejercicio, la Clase PrimeClass solo contiene la implementación
-    del Método de Consulta "sum(num_list)".
+    Para propositos del Ejercicio, la Clase PrimeClass
+    solo contiene la implementacion del Metodo de Consulta
+    "sum(num_list)".
     """
 
     def sum(self, num_list):
         """
-        El Método sum es una clásica Suma Acumulada.
+        El Metodo sum es una clasica Suma Acumulada.
+
+        Retorna: El Entero Acumulado 'acum'.
         """
-        # your sum code here
-        a=0
+        acum = 0
 
-        for i in num_list:
-        	a = a + i
+        for num in num_list:
+            acum = acum + num
 
-        return a
-        
+        return acum
--- primes.py
+++ primes.py
@@ -1,33 +1,41 @@
 """
 "primes.py" por Pedro A. Carpio
 
-Contacteme a través del Teléfono: (416)434-7903, o el Correo Electrónico: alcarpio15@gmail.com
+Contacteme a traves del Telefono: (416)434-7903,
+o el Correo Electronico: alcarpio15@gmail.com
 """
 
 
 class PrimeClass(object):
     """
-    Hasta ahora, la Clase PrimeClass solo contiene la implementación
-    del Método de Consulta "is_prime(num_int)".
+    Hasta ahora, la Clase PrimeClass solo contiene la implementacion
+    del Metodo de Consulta "is_prime(num_int)".
     """
 
     def is_prime(self, num_int):
         """
-        El Método is_prime consiste simplemente del uso liberal de una Bandera Booleana,
-        y tanto la operación "num_int MOD i" como un 'break' dentro de un ciclo tipo-Para:
-        
-        Se asume que el número puede ser primo antes de iniciar el ciclo; y si durante el
-        recorrido se descubre que es multiplo de algún número i, entonces la Bandera reflejará
-        la contradicción antes de que el ciclo frene por la aplicación del break.
+        El Metodo is_prime consiste simplemente del uso liberal de una Bandera
+        Booleana, y tanto la operacion "num_int MOD num" como un 'break' dentro
+        de un ciclo tipo-Para:
 
-        Retorna: la Bandera Booleana 'prim'.
+        Se asume que el numero puede ser primo antes de iniciar el ciclo
+        (pero despues de Evaluar que el numero en cuestión no sea '1'); y si
+        durante el recorrido se descubre que es multiplo de algun numero num,
+        entonces la Bandera reflejara la contradiccion antes de que el ciclo
+        frene por la aplicacion del break.
+
+        Retorna: Un Valor Booleano.
         """
         # your primes code here
         prim = True
 
-        for i in range(2, num_int):
-        	if (num_int%i == 0):
-        		prim = False
-        		break
+        if num_int is 1:
+            return False
 
-        return prim
+        else:
+            for num in range(2, num_int):
+                if num_int % num == 0:
+                    prim = False
+                    break
+
+            return prim
