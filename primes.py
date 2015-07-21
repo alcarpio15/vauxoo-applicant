338b57ef630305cb74d0a98253ddd6682d7b90d1
alcarpio15 <alcarpio15@gmail.com>
2015-07-21 06:46:31 -0430

    Carpio: se Modificó primes.py

--- primes.py
+++ primes.py
@@ -1,16 +1,33 @@
 """
-Your module documentation here
+"primes.py" por Pedro A. Carpio
+
+Contacteme a través del Teléfono: (416)434-7903, o el Correo Electrónico: alcarpio15@gmail.com
 """
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Hasta ahora, la Clase PrimeClass solo contiene la implementación
+    del Método de Consulta "is_prime(num_int)".
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        El Método is_prime consiste simplemente del uso liberal de una Bandera Booleana,
+        y tanto la operación "num_int MOD i" como un 'break' dentro de un ciclo tipo-Para:
+        
+        Se asume que el número puede ser primo antes de iniciar el ciclo; y si durante el
+        recorrido se descubre que es multiplo de algún número i, entonces la Bandera reflejará
+        la contradicción antes de que el ciclo frene por la aplicación del break.
+
+        Retorna: la Bandera Booleana 'prim'.
         """
         # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        prim = True
+
+        for i in range(2, num_int):
+        	if (num_int%i == 0):
+        		prim = False
+        		break
+
+        return prim
